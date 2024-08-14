65acc4584ea8dd22520252b2511c065c20366407
5ock <ccrubio5@gmail.com>
2024-08-14 15:24:29 +0800

    u

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,6 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
  
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    basePath: '/dashboard'
-};
+const nextConfig = {};
  
 export default withNextIntl(nextConfig);
